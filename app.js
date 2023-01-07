d8125dd2ca4e66be75562ab088a890b3ee339fab
lefteris91 <lefteris-kotsialis91@hotmail.com>
2023-01-07 22:38:19 +0200

    modal

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // elements
 const topBtn = document.getElementById('top-btn')
 const header = document.getElementById('header')
+const modal = document.getElementById('modal')
+const modalCloseBtn =document.getElementById('modal-close-btn')
+const modalLink1 = document.getElementById('modal-link-1')
+const modalLink2 = document.getElementById('modal-link-2')
 var sticky = header.offsetTop;
 // 
 // functions
@@ -18,6 +22,28 @@ var sticky = header.offsetTop;
         document.body.scrollTop = 0; //   for safari
         document.documentElement.scrollTop = 0; // rest browsers
   }
+  function closeBtn(){
+    modalCloseBtn.disabled = false
+    modalCloseBtn.addEventListener('click', ()=>{
+      modal.classList.remove('displayN')
+      modal.classList.add('displayY')
+      enableA()
+      
+    })
+  }
+  function disableA(){
+    document.querySelectorAll('a').forEach(a =>{
+      a.classList.add('disabled')
+    })
+    modalLink1.classList.remove('disabled')
+    modalLink2.classList.remove('disabled')
+  }
+  function enableA(){
+    document.querySelectorAll('a').forEach(a =>{
+      a.classList.remove('disabled')
+    })
+  }
+  
 // 
 
 
@@ -26,7 +52,20 @@ window.onscroll = function(){OnScroll()}
 topBtn.addEventListener('click',()=>{
     goTop()
 })
-// topBtn.addEventListener('click' , goTop())
+
+setTimeout(() => {
+  
+  modal.classList.add('displayN')
+  modal.classList.remove('displayY') //modal reveal
+  disableA()
+}, 3000);
+
+setTimeout(()=>{
+  closeBtn()
+  modalCloseBtn.style = 'color:red; cursor:pointer'
+}, 6000)
+
 // 
+ 
 
     
